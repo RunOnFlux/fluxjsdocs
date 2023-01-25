f8addf14fde22206c2b81a9c8abf882cdd6bf3e7
RunOnFlux <runonfluxbot@gmail.com>
2023-01-25 01:14:32 +0000

    Update from https://github.com/RunOnFlux/flux/commit/567e4a5b8d819f442c90110094c429f14b38a94c

--- services/explorerService.js
+++ services/explorerService.js
@@ -690,6 +690,11 @@ async function processBlock(blockHeight, isInsightExplorer) {
           appsService.reinstallOldApplications();
         }
       }
+      if (blockHeight % config.fluxapps.restorePortsSupportPeriod === 0) {
+        if (blockDataVerbose.height >= config.fluxapps.epochstart) {
+          appsService.restorePortsSupport();
+        }
+      }
     }
     const scannedHeight = blockDataVerbose.height;
     // update scanned Height in scannedBlockHeightCollection
