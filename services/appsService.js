581bc8fc1885299db021730c0d08fa8d72794292
RunOnFlux <runonfluxbot@gmail.com>
2023-07-18 13:02:54 +0000

    Update from https://github.com/RunOnFlux/flux/commit/3289298a46821c4426fc8d5555b0accbc68dbe8a

--- services/appsService.js
+++ services/appsService.js
@@ -9246,8 +9246,7 @@ async function forceAppRemovals() {
     });
 
     // array of unique main app names
-    let dockerAppsTrueNameB = [...new Set(dockerAppsTrueNames)];
-    dockerAppsTrueNameB = dockerAppsTrueNameB.filter((appName) => appName !== 'watchtower');
+    const dockerAppsTrueNameB = [...new Set(dockerAppsTrueNames)];
     // eslint-disable-next-line no-restricted-syntax
     for (const dApp of dockerAppsTrueNameB) {
       // check if app is in installedApps
@@ -9598,7 +9597,6 @@ async function checkMyAppsAvailability() {
     const min = minPort;
     const max = maxPort;
     testingPort = failedPort || Math.floor(Math.random() * (max - min) + min);
-    log.info(`checkMyAppsAvailability testing port ${testingPort}.`);
     const iBP = fluxNetworkHelper.isPortBanned(testingPort);
     if (iBP) {
       failedPort = null;
