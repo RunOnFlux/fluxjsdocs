a7af1f6a29e5a53f3f97c9ff846e0edef8570ca9
RunOnFlux <runonfluxbot@gmail.com>
2023-08-30 20:26:53 +0000

    Update from https://github.com/RunOnFlux/flux/commit/5934153860b41b26c65d7b8b5889236b725b1222

--- services/fluxNetworkHelper.js
+++ services/fluxNetworkHelper.js
@@ -287,11 +287,10 @@ async function isFluxAvailable(ip, port = config.server.apiport) {
     if (!UIok) return false;
 
     const syncthingPort = +port + 2;
-    // eslint-disable-next-line no-use-before-define
-    const syncthingOpen = await isPortOpen(ip, syncthingPort);
-    if (!syncthingOpen) return false;
-
-    return true;
+    const exec = `nc -w 2 -z -v ${ip} ${syncthingPort} </dev/null; echo $?`;
+    const cmdAsync = util.promisify(nodecmd.get);
+    const result = await cmdAsync(exec);
+    return !+result;
   } catch (e) {
     return false;
   }
