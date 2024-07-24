7429253665e293ca8474d2e2bf8613fd214b2790
RunOnFlux <runonfluxbot@gmail.com>
2024-07-24 08:53:13 +0000

    Update from https://github.com/RunOnFlux/flux/commit/3ddcd5207a09688d36bc058953fc2bb294473cd4

---
+++ services/utils/fluxRepository.js
@@ -0,0 +1,62 @@
+const path = require('node:path');
+const os = require('node:os');
+const { simpleGit, CleanOptions, ResetMode } = require('simple-git');
+
+class FluxRepository {
+  defaultRepoDir = path.join(os.homedir(), 'zelflux');
+
+  constructor(options = {}) {
+    this.repoPath = options.repoDir || this.defaultRepoDir;
+
+    const gitOptions = {
+      baseDir: this.repoPath,
+      binary: 'git',
+      maxConcurrentProcesses: 6,
+      trimmed: true,
+    };
+
+    this.git = simpleGit(gitOptions);
+  }
+
+  async remotes() {
+    return this.git.getRemotes(true).catch(() => []);
+  }
+
+  async currentBranch() {
+    const branches = await this.git.branch().catch(() => null);
+    if (!branches) return null;
+
+    const { current, detached } = branches;
+
+    return detached ? null : current;
+  }
+
+  async switchBranch(branch, options = {}) {
+    const forceClean = options.forceClean || false;
+    const reset = options.reset || false;
+    const remote = options.remote || 'origin';
+
+    if (forceClean) {
+      await this.git.clean(CleanOptions.FORCE + CleanOptions.RECURSIVE);
+    }
+
+    if (reset) {
+      await this.git.reset(ResetMode.HARD);
+    }
+
+    await this.git.fetch(remote, branch);
+
+    await this.git.checkout(branch);
+  }
+}
+
+async function main() {
+  const repo = new FluxRepository();
+  await repo.switchBranch('noexist');
+}
+
+if (require.main === module) {
+  main();
+}
+
+module.exports = { FluxRepository };
