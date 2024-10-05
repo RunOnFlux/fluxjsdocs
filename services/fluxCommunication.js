e7a4c3c514da1992a36d6772923e0f7c7f267403
RunOnFlux <runonfluxbot@gmail.com>
2024-10-05 18:35:07 +0000

    Update from https://github.com/RunOnFlux/flux/commit/1829662562408e424f9ec4b6e005239162703e1f

--- services/fluxCommunication.js
+++ services/fluxCommunication.js
@@ -682,8 +682,8 @@ async function initiateAndHandleConnection(connection) {
         zlibDeflateOptions: {
         // See zlib defaults.
           chunkSize: 1024,
-          memLevel: 9,
-          level: 9,
+          memLevel: 8,
+          level: 3,
         },
         zlibInflateOptions: {
           chunkSize: 10 * 1024,
@@ -1025,10 +1025,6 @@ async function fluxDiscovery() {
       const fixedIndex = fluxNodeIndex + i < sortedNodeList.length ? fluxNodeIndex + i : fluxNodeIndex + i - sortedNodeList.length;
       const { ip } = sortedNodeList[fixedIndex];
       const ipInc = ip.split(':')[0];
-      if (ipInc === myIP.split(':')[0]) {
-        // eslint-disable-next-line no-continue
-        continue;
-      }
       const portInc = ip.split(':')[1] || '16127';
       // additional precaution
       const clientExists = outgoingConnections.find((client) => client.ip === ipInc && client.port === portInc);
@@ -1045,10 +1041,6 @@ async function fluxDiscovery() {
       const fixedIndex = fluxNodeIndex - i > 0 ? fluxNodeIndex - i : sortedNodeList.length - fluxNodeIndex - i;
       const { ip } = sortedNodeList[fixedIndex];
       const ipInc = ip.split(':')[0];
-      if (ipInc === myIP.split(':')[0]) {
-        // eslint-disable-next-line no-continue
-        continue;
-      }
       const portInc = ip.split(':')[1] || '16127';
       // additional precaution
       const clientExists = outgoingConnections.find((client) => client.ip === ipInc && client.port === portInc);
