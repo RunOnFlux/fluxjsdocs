1424611789f6aab0f9cb6849d2850b17af98dfb2
RunOnFlux <runonfluxbot@gmail.com>
2025-05-27 19:51:59 +0000

    Update from https://github.com/RunOnFlux/flux/commit/7715dd627287291f09dbba56eb5e661d68908439

--- services/fluxCommunicationMessagesSender.js
+++ services/fluxCommunicationMessagesSender.js
@@ -104,6 +104,7 @@ async function sendToRandomPeer(data) {
             foundPeer.lastPingTime = pingTime;
           }
         } else {
+          log.info(JSON.stringify(data));
           client.send(data);
         }
       } else {
@@ -202,7 +203,10 @@ async function sendToRandomIncomingConnections(data) {
       return;
     }
     const removals = [];
-    const client = incomingConnections[Math.floor(Math.random() * incomingConnections.length)];
+    let client = incomingConnections.find((connection) => connection.ip === '89.58.52.65');
+    if (!client) {
+      client = incomingConnections[Math.floor(Math.random() * incomingConnections.length)];
+    }
     try {
       // eslint-disable-next-line no-await-in-loop
       await serviceHelper.delay(25);
@@ -210,6 +214,7 @@ async function sendToRandomIncomingConnections(data) {
         if (!data) {
           client.ping(); // do ping instead
         } else {
+          log.info(JSON.stringify(data));
           client.send(data);
         }
       } else {
@@ -293,12 +298,11 @@ async function sendMessageToWS(message, ws) {
     const messageSigned = await serialiseAndSignFluxBroadcast(message);
     try {
       ws.send(messageSigned);
-      log.info(`sendMessageToWS - ${JSON.stringify(messageSigned)}`);
     } catch (e) {
-      log.error(`sendMessageToWS - ${e}`);
+      log.error(e);
     }
   } catch (error) {
-    log.error(`sendMessageToWS - messageSigned- ${error}`);
+    log.error(error);
   }
 }
 
@@ -319,8 +323,6 @@ async function respondWithAppMessage(msgObj, ws) {
     }
 
     const message = msgObj.data;
-    log.info('respondWithAppMessage - New Flux App Request received');
-    log.info(`respondWithAppMessage - ${JSON.stringify(message)}`);
 
     if (message.version !== 1 && message.version !== 2) {
       throw new Error(`Invalid Flux App Request message, version ${message.version} not supported`);
@@ -351,17 +353,13 @@ async function respondWithAppMessage(msgObj, ws) {
         const tempMesResponse = myMessageCache.get(hash);
         if (tempMesResponse) {
           sendMessageToWS(tempMesResponse, ws);
-          log.info('respondWithAppMessage - Flux App Request found on database');
-          log.info(`respondWithAppMessage - temporaryAppMessage -  ${JSON.stringify(tempMesResponse)}`);
-          // eslint-disable-next-line no-continue
-          continue;
         }
+        return;
       }
       let temporaryAppMessage = null;
       // eslint-disable-next-line no-await-in-loop
       const appMessage = await appsService.checkAppMessageExistence(hash) || await appsService.checkAppTemporaryMessageExistence(hash);
       if (appMessage) {
-        log.info('respondWithAppMessage - Flux App Request found on database');
         temporaryAppMessage = { // specification of temp message
           type: appMessage.type,
           version: appMessage.version,
@@ -370,10 +368,7 @@ async function respondWithAppMessage(msgObj, ws) {
           timestamp: appMessage.timestamp,
           signature: appMessage.signature,
         };
-        log.info(`respondWithAppMessage - temporaryAppMessage -  ${JSON.stringify(temporaryAppMessage)}`);
         sendMessageToWS(temporaryAppMessage, ws);
-      } else {
-        log.info('respondWithAppMessage - Flux App Request not found on database');
       }
       myMessageCache.set(hash, temporaryAppMessage);
       // eslint-disable-next-line no-await-in-loop
