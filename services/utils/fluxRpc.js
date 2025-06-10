a7c3541c849837ebcba8df2fe37ec4605dd0d090
RunOnFlux <runonfluxbot@gmail.com>
2025-06-10 14:41:28 +0000

    Update from https://github.com/RunOnFlux/flux/commit/df71d29bb62a140dc48ef8e83a69a3c5aaa564c7

--- services/utils/fluxRpc.js
+++ services/utils/fluxRpc.js
@@ -192,8 +192,6 @@ class FluxRpc {
       ['getinfo', null],
       ['decryptmessage', null],
       ['getpublickey', null],
-      ['decryptrsamessage', null],
-      ['encryptmessage', null],
     ]),
   };
 
