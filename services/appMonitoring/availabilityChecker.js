6952f7685b3086b824f734bd1ca7ab1e7a0d3c65
RunOnFlux <runonfluxbot@gmail.com>
2025-11-03 12:56:47 +0000

    Update from https://github.com/RunOnFlux/flux/commit/c27f9ef8bbe7a0c87e444799b7bdca30abdb9612

--- services/appMonitoring/availabilityChecker.js
+++ services/appMonitoring/availabilityChecker.js
@@ -9,7 +9,6 @@ const daemonServiceMiscRpcs = require('../daemonService/daemonServiceMiscRpcs');
 const upnpService = require('../upnpService');
 const networkStateService = require('../networkStateService');
 const fluxHttpTestServer = require('../utils/fluxHttpTestServer');
-const { decryptEnterpriseApps } = require('../appQuery/appQueryService');
 const log = require('../../lib/log');
 
 // Helper function to sign check app data
@@ -137,9 +136,6 @@ async function checkMyAppsAvailability(installedAppsFn, dosState, portsNotWorkin
       setImmediate(() => checkMyAppsAvailability(installedAppsFn, dosState, portsNotWorking, failedNodesTestPortsCache, isArcane));
       return;
     }
-
-    // Decrypt enterprise apps (version 8 with encrypted content)
-    installedAppsRes.data = await decryptEnterpriseApps(installedAppsRes.data);
 
     const apps = installedAppsRes.data;
     const appPorts = [];
