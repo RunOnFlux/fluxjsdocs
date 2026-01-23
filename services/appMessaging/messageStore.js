9efd75991ed359ac0b7a04b02b523073b6a7060b
RunOnFlux <runonfluxbot@gmail.com>
2026-01-23 10:29:50 +0000

    Update from https://github.com/RunOnFlux/flux/commit/297f0c92bb25855118276f8335c5169fa57336a3

--- services/appMessaging/messageStore.js
+++ services/appMessaging/messageStore.js
@@ -8,7 +8,6 @@ const appValidator = require('../appRequirements/appValidator');
 const registryManager = require('../appDatabase/registryManager');
 // const advancedWorkflows = require('../appLifecycle/advancedWorkflows'); // Moved to dynamic require to avoid circular dependency
 const { checkAndDecryptAppSpecs } = require('../utils/enterpriseHelper');
-const globalState = require('../utils/globalState');
 const {
   globalAppsMessages,
   globalAppsTempMessages,
@@ -88,20 +87,6 @@ async function storeAppTemporaryMessage(message, furtherVerification = false) {
     // eslint-disable-next-line global-require
     const advancedWorkflows = require('../appLifecycle/advancedWorkflows');
     const appRegistration = message.type === 'zelappregister' || message.type === 'fluxappregister';
-
-    // For updates, fetch previous app specs first - if registration doesn't exist yet, queue the update
-    let previousAppSpecs = null;
-    if (!appRegistration) {
-      previousAppSpecs = await advancedWorkflows.getPreviousAppSpecifications(appSpecFormatted, messageTimestamp);
-      if (!previousAppSpecs) {
-        // Registration doesn't exist yet - queue this update for later processing
-        const appName = appSpecFormatted.name;
-        log.info(`Queueing update for ${appName} - registration not yet stored`);
-        globalState.queuePendingUpdate(appName, message, block);
-        return false; // Don't rebroadcast - we'll process when registration arrives
-      }
-    }
-
     if (appSpecFormatted.version >= 8 && appSpecFormatted.enterprise) {
       if (!message.arcaneSender) {
         return new Error('Invalid Flux App message for storing, enterprise app where original sender was not arcane node');
@@ -120,7 +105,7 @@ async function storeAppTemporaryMessage(message, furtherVerification = false) {
         if (appRegistration) {
           await registryManager.checkApplicationRegistrationNameConflicts(appSpecFormattedDecrypted, message.hash);
         } else {
-          await advancedWorkflows.validateApplicationUpdateCompatibility(appSpecFormattedDecrypted, previousAppSpecs);
+          await advancedWorkflows.validateApplicationUpdateCompatibility(appSpecFormattedDecrypted, messageTimestamp);
         }
       }
     } else {
@@ -128,7 +113,7 @@ async function storeAppTemporaryMessage(message, furtherVerification = false) {
       if (appRegistration) {
         await registryManager.checkApplicationRegistrationNameConflicts(appSpecFormatted, message.hash);
       } else {
-        await advancedWorkflows.validateApplicationUpdateCompatibility(appSpecFormatted, previousAppSpecs);
+        await advancedWorkflows.validateApplicationUpdateCompatibility(appSpecFormatted, messageTimestamp);
       }
     }
 
@@ -136,7 +121,8 @@ async function storeAppTemporaryMessage(message, furtherVerification = false) {
     if (appRegistration) {
       await messageVerifier.verifyAppMessageSignature(message.type, messageVersion, appSpecFormatted, messageTimestamp, message.signature);
     } else {
-      // previousAppSpecs already fetched above for update validation
+      // get previousAppSpecifications as we need previous owner
+      const previousAppSpecs = await advancedWorkflows.getPreviousAppSpecifications(appSpecFormatted, messageTimestamp);
       const { owner } = previousAppSpecs;
       // here signature is checked against PREVIOUS app owner
       await messageVerifier.verifyAppMessageUpdateSignature(message.type, messageVersion, appSpecFormatted, messageTimestamp, message.signature, owner, block);
--- services/appRequirements/appValidator.js
+++ services/appRequirements/appValidator.js
@@ -53,6 +53,7 @@ function verifyTypeCorrectnessOfApp(appSpecification) {
     expire,
     nodes,
     staticip,
+    datacenter,
     enterprise,
   } = appSpecification;
 
@@ -530,6 +531,20 @@ function verifyTypeCorrectnessOfApp(appSpecification) {
 
     if (typeof staticip !== 'boolean') {
       throw new Error('Invalid static ip value obtained. Only boolean as true or false allowed.');
+    }
+  }
+
+  // v8+ datacenter validation
+  if (version >= 8) {
+    if (datacenter !== null && datacenter !== true && datacenter !== false) {
+      throw new Error('Invalid datacenter value obtained. Only null, true, or false allowed.');
+    }
+    // datacenter=true is only allowed for enterprise app owners
+    if (datacenter === true) {
+      const enterpriseAppOwners = config.enterpriseAppOwners || [];
+      if (!enterpriseAppOwners.includes(owner)) {
+        throw new Error('Datacenter requirement is only available for enterprise app owners.');
+      }
     }
   }
 
@@ -1031,7 +1046,7 @@ function verifyObjectKeysCorrectnessOfApp(appSpecifications) {
   } else if (appSpecifications.version === 8) {
     const specifications = [
       'version', 'name', 'description', 'owner', 'compose', 'instances', 'contacts',
-      'geolocation', 'expire', 'nodes', 'staticip', 'enterprise',
+      'geolocation', 'expire', 'nodes', 'staticip', 'datacenter', 'enterprise',
     ];
     const componentSpecifications = [
       'name', 'description', 'repotag', 'ports', 'containerPorts', 'environmentParameters', 'commands', 'containerData', 'domains', 'repoauth',
@@ -1444,11 +1459,7 @@ async function verifyAppUpdateParameters(req, res) {
       // Dynamic require to avoid circular dependency
       // eslint-disable-next-line global-require
       const advancedWorkflows = require('../appLifecycle/advancedWorkflows');
-      const previousAppSpecs = await advancedWorkflows.getPreviousAppSpecifications(appSpecFormatted, timestamp);
-      if (!previousAppSpecs) {
-        throw new Error(`Flux App ${appSpecFormatted.name} does not exist and cannot be updated`);
-      }
-      await advancedWorkflows.validateApplicationUpdateCompatibility(appSpecFormatted, previousAppSpecs);
+      await advancedWorkflows.validateApplicationUpdateCompatibility(appSpecFormatted, timestamp);
 
       if (isEnterprise) {
         appSpecFormatted.contacts = [];
