5f6d6cfeb7e019280082db804dccf9bae6c5f14a
RunOnFlux <runonfluxbot@gmail.com>
2026-07-03 14:03:56 +0000

    Update from https://github.com/RunOnFlux/flux/commit/2b734d12905bcc90c3d49bc64eedacd9c3a7830f

--- services/appMonitoring/syncthingMonitorHelpers.js
+++ services/appMonitoring/syncthingMonitorHelpers.js
@@ -1,7 +1,5 @@
 // Syncthing Monitor - Helper Functions
 const axios = require('axios');
-const fs = require('node:fs/promises');
-const path = require('node:path');
 const log = require('../../lib/log');
 const serviceHelper = require('../serviceHelper');
 const volumeService = require('../utils/volumeService');
@@ -217,18 +215,11 @@ async function ensureStfolderExists(folder) {
     log.error(`ensureStfolderExists - ${folder} is not a mountpoint; refusing to create .stfolder on the bare directory`);
     return false;
   }
-  // creation is a one-time setup act: when the marker is already present in
-  // the mounted volume there is nothing to do (and nothing to log)
-  const marker = path.join(folder, '.stfolder');
-  const exists = await fs.stat(marker).then((stats) => stats.isDirectory()).catch(() => false);
-  if (exists) return true;
-
-  const mkdir = await serviceHelper.runCommand('mkdir', { runAsRoot: true, params: ['-p', marker] });
+  const mkdir = await serviceHelper.runCommand('mkdir', { runAsRoot: true, params: ['-p', `${folder}/.stfolder`] });
   if (mkdir.error) {
     log.error(`ensureStfolderExists - failed to create .stfolder in ${folder}: ${mkdir.error.message}`);
     return false;
   }
-  log.info(`ensureStfolderExists - created .stfolder in ${folder}`);
   return true;
 }
 
