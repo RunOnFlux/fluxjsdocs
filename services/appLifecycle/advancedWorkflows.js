1210b6aa4d7d237200f0d596b6cfa93bb99671a1
RunOnFlux <runonfluxbot@gmail.com>
2026-08-11 15:15:53 +0000

    Update from https://github.com/RunOnFlux/flux/commit/a2eb0a094ac7b3992c3d605df63cad86fe31307e

--- services/appLifecycle/advancedWorkflows.js
+++ services/appLifecycle/advancedWorkflows.js
@@ -1,6 +1,5 @@
 const config = require('config');
 const util = require('util');
-const df = require('node-df');
 const fs = require('node:fs');
 const path = require('node:path');
 const nodecmd = require('node-cmd');
@@ -28,6 +27,7 @@ const {
   appsFolder,
   appVolumesPath,
   legacyAppVolumesPath,
+  APP_VOLUME_MOUNT_OPTIONS,
 } = require('../utils/appConstants');
 const { specificationFormatter } = require('../utils/appSpecHelpers');
 const { compareInstanceSeniority } = require('../utils/instanceOrdering');
@@ -414,7 +414,6 @@ let dosMountMessage = '';
  * @returns {Promise<void>}
  */
 async function createAppVolume(appSpecifications, appName, isComponent, res) {
-  const dfAsync = util.promisify(df);
   const identifier = isComponent ? `${appSpecifications.name}_${appName}` : appName;
   const appId = dockerService.getAppIdentifier(identifier);
 
@@ -427,22 +426,7 @@ async function createAppVolume(appSpecifications, appName, isComponent, res) {
     if (res.flush) res.flush();
   }
 
-  // we want whole numbers in GB
-  const options = {
-    prefixMultiplier: 'GB',
-    isDisplayPrefixMultiplier: false,
-    precision: 0,
-  };
-
-  const dfres = await dfAsync(options);
-  const okVolumes = [];
-  dfres.forEach((volume) => {
-    if (volume.filesystem.includes('/dev/') && !volume.filesystem.includes('loop') && !volume.mount.includes('boot')) {
-      okVolumes.push(volume);
-    } else if (volume.filesystem.includes('loop') && volume.mount === '/') {
-      okVolumes.push(volume);
-    }
-  });
+  const okVolumes = await volumeService.capacityVolumesInGib();
 
   // Dynamic require to avoid circular dependency
   // eslint-disable-next-line global-require
@@ -601,7 +585,7 @@ async function createAppVolume(appSpecifications, appName, isComponent, res) {
       res.write(serviceHelper.ensureString(mountingStatus));
       if (res.flush) res.flush();
     }
-    await execAsRoot('mount', ['-o', 'loop', volumeFile, appDir]);
+    await execAsRoot('mount', ['-o', APP_VOLUME_MOUNT_OPTIONS, volumeFile, appDir]);
     const mountingStatus2 = {
       status: 'Volume mounted',
     };
@@ -2455,28 +2439,12 @@ async function testAppMount() {
     await removeTestAppMount();
     const appSize = 1;
     const overHeadRequired = 2;
-    const dfAsync = util.promisify(df);
     const appId = 'flux_fluxTestVol';
 
     log.info('Mount Test: started');
     log.info('Mount Test: Searching available space...');
 
-    // we want whole numbers in GB
-    const options = {
-      prefixMultiplier: 'GB',
-      isDisplayPrefixMultiplier: false,
-      precision: 0,
-    };
-
-    const dfres = await dfAsync(options);
-    const okVolumes = [];
-    dfres.forEach((volume) => {
-      if (volume.filesystem.includes('/dev/') && !volume.filesystem.includes('loop') && !volume.mount.includes('boot')) {
-        okVolumes.push(volume);
-      } else if (volume.filesystem.includes('loop') && volume.mount === '/') {
-        okVolumes.push(volume);
-      }
-    });
+    const okVolumes = await volumeService.capacityVolumesInGib();
 
     // check if space is not sharded in some bad way. Always count the fluxSystemReserve
     let useThisVolume = null;
@@ -2519,7 +2487,7 @@ async function testAppMount() {
     log.info('Mount Test: Directory made');
     log.info('Mount Test: Mounting volume...');
 
-    await execAsRoot('mount', ['-o', 'loop', volumePath, path.join(appsFolder, appId)]);
+    await execAsRoot('mount', ['-o', APP_VOLUME_MOUNT_OPTIONS, volumePath, path.join(appsFolder, appId)]);
     log.info('Mount Test: Volume mounted. Test completed.');
     dosMountMessage = '';
     // run removal
