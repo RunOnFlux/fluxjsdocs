47908b2d6da35f7e030a4c1130c152fdafb8ea0e
RunOnFlux <runonfluxbot@gmail.com>
2026-08-15 06:11:32 +0000

    Update from https://github.com/RunOnFlux/flux/commit/a2edf5a4847b9bfa276dd8228d6e4fcab6174c97

--- services/syncthingService.js
+++ services/syncthingService.js
@@ -2355,10 +2355,6 @@ async function adjustSyncthing() {
         }
       }
     }
-    const restartRequired = await getConfigRestartRequired();
-    if (restartRequired.status === 'success' && restartRequired.data.requiresRestart === true) {
-      await systemRestart();
-    }
   } catch (error) {
     log.error(error);
   }
